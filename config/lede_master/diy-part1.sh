45758834ce836ac1392cb11af5fb67a676be4c7f
Qoo-330ml <51946091+Qoo-330ml@users.noreply.github.com>
2026-04-22 09:17:19 +0800

    Update diy-part1.sh

--- config/lede_master/diy-part1.sh
+++ config/lede_master/diy-part1.sh
@@ -12,3 +12,5 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
+git clone https://github.com/FUjr/QModem.git package/lean/luci-app-qmodem
+git clone https://github.com/FUjr/QModem.git package/lean/luci-app-qmodem-sms
