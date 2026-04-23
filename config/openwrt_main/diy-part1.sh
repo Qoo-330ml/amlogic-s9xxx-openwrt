b75d317da504f32935b3ef3d08d2fc41607b5c11
Qoo-330ml <51946091+Qoo-330ml@users.noreply.github.com>
2026-04-23 09:38:57 +0800

    Update diy-part1.sh

--- config/openwrt_main/diy-part1.sh
+++ config/openwrt_main/diy-part1.sh
@@ -12,3 +12,6 @@
 # Remove unnecessary packages
 # rm -rf package/utils/{ucode,fbtest}
 
+git clone https://github.com/FUjr/QModem.git package/lean/luci-app-qmodem
+git clone https://github.com/FUjr/QModem.git package/lean/luci-app-qmodem-sms
+# git clone https://github.com/FUjr/QModem.git package/lean/luci-app-qmodem-next
